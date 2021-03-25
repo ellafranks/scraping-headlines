1a5b71f6b280a696987f4e79f64e330cd9c28463
Ella D. Franks <ella.franks97@gmail.com>
2021-03-25 16:45:18 +0000

    2nd commit

--- scripts/test.py
+++ scripts/test.py
@@ -1,5 +1,9 @@
 from dailymail import gather_headlines
 from dailymail.gather_headlines import generate_urls
+from dailymail.read_data import combine_json, make_df
+
 
 if __name__ == '__main__':
-    generate_urls('/Users/ella.franks/PycharmProjects/news-headlinesProject/data/')
+    # generate_urls('/Users/ella.franks/PycharmProjects/news-headlinesProject/data/')
+    make_df(
+        combine_json('/Users/ella.franks/PycharmProjects/news-headlinesProject/data/*json'))
