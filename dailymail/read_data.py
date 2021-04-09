4016f3b714f7b2f79dc0b31654df6aa00c3478d1
Ella D. Franks <ella.franks97@gmail.com>
2021-04-09 13:52:49 +0100

    4th commit

--- dailymail/read_data.py
+++ dailymail/read_data.py
@@ -2,17 +2,22 @@ import glob
 import json
 import pandas as pd
 import re
+import uuid
+
+from utils import url_to_headline
 
 
-def combine_json(path):
+def combine_json_files(path):
+    full_dict = {}
     for file in glob.glob(path):
         data = json.load(open(file))
-    return data
+        for _, values in data.items():
+            full_dict[uuid.uuid4()] = values
+    return full_dict
 
 
 def make_df(data):
     df = pd.DataFrame.from_dict(data, orient='index')
-    df.url = df.url.apply(lambda x: re.findall('([^\/]+)(?=.html)', x)[0].replace('-', ' '))
-    print(df.head())
+    df['headline'] = df.url.apply(url_to_headline)
+    df['url'] = df.url.apply(lambda x: 'dailymail.co.uk' + x)
     return df
-
--- scripts/test.py
+++ scripts/test.py
@@ -1,9 +1,15 @@
-from dailymail import gather_headlines
 from dailymail.gather_headlines import generate_urls
-from dailymail.read_data import combine_json, make_df
+from dailymail.read_data import make_df, combine_json_files
+
+
+def execute(path):
+    # generate_urls(path)
+    data = combine_json_files(path + '*json')
+    df = make_df(data)
+    print(df.head())
+    return df
 
 
 if __name__ == '__main__':
     # generate_urls('/Users/ella.franks/PycharmProjects/news-headlinesProject/data/')
-    make_df(
-        combine_json('/Users/ella.franks/PycharmProjects/news-headlinesProject/data/*json'))
+    execute('/Users/ella.franks/PycharmProjects/news-headlinesProject/data/')
